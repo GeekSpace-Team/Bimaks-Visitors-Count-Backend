306a9670635ad75183b243072054c5c669ccbbc0
Halil777 <microsoft779@gmail.com>
2024-08-15 16:06:40 +0500

    change url

--- server.js
+++ server.js
@@ -5,39 +5,40 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
+// CORS configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "http://95.85.121.153:7757/", // The origin allowed to make requests
   methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors("*"));
+app.use(cors(corsOptions));
 app.use(express.json());
 
 let visitorCount = 0;
 const adminToken = "bimaksAdminToken";
 
-const checkAdmin = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (token === `Bearer ${adminToken}`) {
-    next();
-  } else {
-    res.status(403).json({ message: "Forbidden" });
-  }
-};
-
-app.get("/api/visitor-count/:siteId", (req, res) => {
+// Route to get visitor count
+app.get("/api/visitor-count", (req, res) => {
   res.json({ count: visitorCount });
 });
 
+// Route to increment visitor count
 app.post("/api/increment-visitor-count", (req, res) => {
   const token = req.headers["authorization"];
-  if (token === `Bearer ${adminToken}`) {
-    res.status(403).json({ message: "Forbidden" });
-  } else {
+  if (token !== `Bearer ${adminToken}`) {
     visitorCount += 1;
+    console.log(`Visitor Count Updated: ${visitorCount}`); // Log to console
     res.json({ count: visitorCount });
+  } else {
+    res.status(403).json({ message: "Forbidden" });
   }
+});
+
+// Route to log each visit (optional)
+app.post("/api/log-visit", (req, res) => {
+  console.log(`Visit logged from IP: ${req.ip}`);
+  res.status(200).send("Visit logged");
 });
 
 app.listen(port, () => {
